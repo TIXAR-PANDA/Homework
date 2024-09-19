d238a7636bd36312eefbf11a2f53aefa74335929
TIXAR-PANDA <natali040168@mail.ru>
2024-09-19 18:57:46 +0300

    Add files via upload

---
+++ module_1_1.py
@@ -0,0 +1,9 @@
+# Практическое задание по работе в Pycharm - "Переменные".
+number_of_homework = 12  # Количество выполненных ДЗ (запишите значение 12)
+elapsed_time = 1.5       # Количество затраченных часов (запишите значение 1.5)
+training_course = 'Python'  # Название курса (запишите значение 'Python')
+# Время на одно задание (вычислить используя 1 и 2 переменные)
+time_for_one_task = elapsed_time / number_of_homework
+
+print("Курс:", training_course, ", всего задач:", number_of_homework, ", затрачено часов:", elapsed_time,
+      ", среднее время выполнения:", time_for_one_task, "часов.")
