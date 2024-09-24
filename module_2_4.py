5af43b7f677f4f3197d2b2d198e36c3c986fe415
TIXAR-PANDA <natali040168@mail.ru>
2024-09-24 06:49:23 +0300

    Add files via upload

--- module_2_4.py
+++ module_2_4.py
@@ -1,15 +1,21 @@
-# Домашняя работа по уроку:"Цикл for. Элементы списка. Полезные функции в цик
+# Домашняя работа по уроку:"Цикл for. Элементы списка. Полезные функции в цикле
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
+is_prime = True
 for i in numbers:
     if i < 2:
         continue
     for j in primes:
         if i % j == 0:
-            not_primes.append(i)
+            is_prime = False
             break
     else:
-            primes.append(i)
+        is_prime = True
+    if not (is_prime):
+        not_primes.append(i)
+    else:
+        primes.append(i)
 print(f'Простые числа: {primes} \nСоставные числа: {not_primes}')
+
