d34c5a6b5a3959da456595dac3a84c2016ae6bc1
TIXAR-PANDA <natali040168@mail.ru>
2024-09-24 09:35:45 +0300

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,10 +1,11 @@
+# Дополнительное практическое задание по второму модулю*
 def pass_word():
     list_value = []  # Список для  пар значений
     list_password = []  # Список для хранения значиний после объеденения списков внутри list_value
     n = int(input('Введите число от 3 до 20: '))
     if n < 3 or n > 20:
         print('Введено неверное число.')
-        print( )
+        print()
         pass_word()
 
     else:
@@ -16,9 +17,9 @@ def pass_word():
                     list_value.append([i, j])  # подобрали уникальные значения пар
 
         for i in list_value:
-             list_password.extend(i)  # распаковали внутренние списки и объеденили их в один список
+            list_password.extend(i)  # распаковали внутренние списки и объеденили их в один список
         password = ''.join(str(x) for x in list_password)  # перебрали значения из списка, присвоили их переменой
         print('пароль : ', password)
 
-pass_word()
 
+pass_word()
