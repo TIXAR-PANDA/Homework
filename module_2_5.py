cd737972eb68767ee161f7ed8050feb944cbd64e
TIXAR-PANDA <natali040168@mail.ru>
2024-09-24 07:51:48 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,14 @@
+#Домашняя работа по уроку "Функции в Python.Функция с параметром"
+
+ def get_matrix(n, m, value):
+    matrix = []
+    for i in range(1, n + 1):
+        line = []
+        matrix.append(line)
+        for j in range(1, m + 1):
+            line.append(value)
+    print(matrix)
+
+get_matrix(2, 4, 10)
+get_matrix(3, 5, 42)
+get_matrix(4, 2, 13)
