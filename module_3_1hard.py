244f9505914a066047376bc5b25e39ed43f435ce
TIXAR-PANDA <natali040168@mail.ru>
2024-10-06 16:26:33 +0300

    Add files via upload

---
+++ module_3_1hard.py
@@ -0,0 +1,52 @@
+# Задание "Раз, два, три, четыре, пять .... Это не всё?":
+
+
+def calculate_structure_sum(structure_):
+    n = len(structure_)
+    ss = 0
+    for i in range(0, n):
+        if isinstance (structure_[i],dict):
+            #'идем к словарям----'
+            keys = list(structure_[i].keys())
+            values_ = list(structure_[i].values())
+            structure_dict = [keys,values_]
+            ss1 = calculate_structure_sum(structure_dict)
+            ss = ss + ss1
+        if isinstance(structure_[i], list):
+            #'идем к спискам-----'
+            ss1 = calculate_structure_sum(structure_[i])
+            ss = ss +  ss1
+
+        if isinstance(structure_[i], int):
+            #'идем к числам'
+            ss = ss + int(structure_[i])
+
+        if isinstance(structure_[i], str):
+            #'идем к строкам'
+            ss = ss + len(structure_[i])
+
+        if isinstance(structure_[i], tuple):
+            #'идем к кортежу'
+            ss1 = calculate_structure_sum(structure_[i])
+            ss = ss + ss1
+
+        if isinstance(structure_[i], set ):
+            #'идем к множеству'
+            structure_1 = list (structure_[i])
+            ss1 = calculate_structure_sum(structure_1)
+            ss = ss + ss1
+
+    return ss
+
+
+
+data_structure = [
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+print ( '  Результат:  ', calculate_structure_sum(data_structure))
+ # Результат:   99
