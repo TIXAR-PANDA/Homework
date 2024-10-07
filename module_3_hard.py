186e96db2994782bd019c2d201b7d0f7771de2d9
TIXAR-PANDA <natali040168@mail.ru>
2024-10-07 18:24:24 +0300

    Update module_3_hard.py

--- module_3_hard.py
+++ module_3_hard.py
@@ -2,50 +2,40 @@
 
 
 def calculate_structure_sum(structure_):
-    print(structure_)
     n = len(structure_)
     ss = 0
     for i in range(0, n):
-        print ('    ', i )
-        print(structure_[i])
-        print(type(structure_[i]))
-
         if isinstance (structure_[i],dict):
-            print ('идем к словарям----')
+            '''идем к словарям----'''
             keys = list(structure_[i].keys())
             values_ = list(structure_[i].values())
             structure_dict = [keys,values_]
-            print(' ------------' ,structure_dict )
             ss1 = calculate_structure_sum(structure_dict)
             ss = ss + ss1
-            print('ss = ', ss)
         if isinstance(structure_[i], list):
-            print('идем к спискам-----')
+            '''идем к спискам-----'''
             ss1 = calculate_structure_sum(structure_[i])
             ss = ss +  ss1
-            print ('ss = ', ss)
+           
         if isinstance(structure_[i], int):
-            print('идем к числам')
+            '''идем к числам '''
             ss = ss + int(structure_[i])
-            print('ss = ', ss)
 
         if isinstance(structure_[i], str):
-            print('идем к строкам')
+           '''идем к строкам'''
             ss = ss + len(structure_[i])
-            print('ss = ', ss)
 
         if isinstance(structure_[i], tuple):
-            print('идем к кортежу')
+           '''идем к кортежу'''
             ss1 = calculate_structure_sum(structure_[i])
             ss = ss + ss1
 
             print('ss = ', ss)
         if isinstance(structure_[i], set ):
-            print('идем к множеству')
+            '''идем к множеству'''
             structure_1 = list (structure_[i])
             ss1 = calculate_structure_sum(structure_1)
             ss = ss + ss1
-    print ('rez = ', ss)
     return ss
 
 
