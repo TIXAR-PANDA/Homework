334d4c97a6a4fce2015986facf88dd85f1f912fb
TIXAR-PANDA <natali040168@mail.ru>
2024-10-07 19:03:09 +0300

    Update module_3_hard.py

--- module_3_hard.py
+++ module_3_hard.py
@@ -5,40 +5,38 @@ def calculate_structure_sum(structure_):
     n = len(structure_)
     ss = 0
     for i in range(0, n):
-       if isinstance (structure_[i],dict):
-            '''идем к словарям----'''
+        if isinstance (structure_[i],dict):
+            #'идем к словарям----'
             keys = list(structure_[i].keys())
             values_ = list(structure_[i].values())
             structure_dict = [keys,values_]
             ss1 = calculate_structure_sum(structure_dict)
             ss = ss + ss1
-           
-        if isinstance(structure_[i], (list,  tuple)):
-            '''идем к спискам , к кортежу -----'''
+        if isinstance(structure_[i], (list , tuple)):
+            '''идем к спискам или к кортежу-----'''
             ss1 = calculate_structure_sum(structure_[i])
             ss = ss +  ss1
-            
+
         if isinstance(structure_[i], int):
-           '''идем к числам'''
+            #'идем к числам'
             ss = ss + int(structure_[i])
-            print('ss = ', ss)
 
         if isinstance(structure_[i], str):
-            '''идем к строкам----'''
+            #'идем к строкам'
             ss = ss + len(structure_[i])
-            print('ss = ', ss)
+
+        #if isinstance(structure_[i], tuple):
+            #'идем к кортежу'
+            #ss1 = calculate_structure_sum(structure_[i])
+            #ss = ss + ss1
 
         if isinstance(structure_[i], set ):
-            '''идем к множеству----'''
+            #'идем к множеству'
             structure_1 = list (structure_[i])
             ss1 = calculate_structure_sum(structure_1)
             ss = ss + ss1
 
     return ss
-
-
-
-
 
 
 
@@ -50,5 +48,5 @@ data_structure = [
 ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
-print( calculate_structure_sum(data_structure) )
-
+print ( '  Результат:  ', calculate_structure_sum(data_structure))
+ # Результат:   99
