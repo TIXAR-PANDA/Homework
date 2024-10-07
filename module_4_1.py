455c8ba982582bad5bf663c6aa6095fa5906b9eb
TIXAR-PANDA <natali040168@mail.ru>
2024-10-07 18:03:15 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+
+# Домашняя работа по уроку "Модули и пакеты"
+
+from fake_math import divide as div1
+from true_math import divide as div0
+print('69 / 3 = ', div1(69, 3))
+print(' 4 /0  = ', div1(4, 0))
+print( )
+print('56 / 5 = ', div0(56, 5))
+print('15 / 0 = ', div0(15, 0))
