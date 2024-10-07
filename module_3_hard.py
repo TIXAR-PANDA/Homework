245dd724632cda7d646ab953f5b2f2f8c2b56c5b
TIXAR-PANDA <natali040168@mail.ru>
2024-10-07 18:55:20 +0300

    Update module_3_hard.py

--- module_3_hard.py
+++ module_3_hard.py
@@ -5,43 +5,35 @@ def calculate_structure_sum(structure_):
     n = len(structure_)
     ss = 0
     for i in range(0, n):
-
        if isinstance (structure_[i],dict):
-            print ('идем к словарям----')
+            '''идем к словарям----'''
             keys = list(structure_[i].keys())
             values_ = list(structure_[i].values())
             structure_dict = [keys,values_]
-            print(' ------------' ,structure_dict )
             ss1 = calculate_structure_sum(structure_dict)
             ss = ss + ss1
-            print('ss = ', ss)
-        if isinstance(structure_[i], list):
-            print('идем к спискам-----')
+           
+        if isinstance(structure_[i], (list,  tuple)):
+            '''идем к спискам , к кортежу -----'''
             ss1 = calculate_structure_sum(structure_[i])
             ss = ss +  ss1
-            print ('ss = ', ss)
+            
         if isinstance(structure_[i], int):
-            print('идем к числам')
+           '''идем к числам'''
             ss = ss + int(structure_[i])
             print('ss = ', ss)
 
         if isinstance(structure_[i], str):
-            print('идем к строкам')
+            '''идем к строкам----'''
             ss = ss + len(structure_[i])
             print('ss = ', ss)
 
-        if isinstance(structure_[i], tuple):
-            print('идем к кортежу')
-            ss1 = calculate_structure_sum(structure_[i])
-            ss = ss + ss1
-
-            print('ss = ', ss)
         if isinstance(structure_[i], set ):
-            print('идем к множеству')
+            '''идем к множеству----'''
             structure_1 = list (structure_[i])
             ss1 = calculate_structure_sum(structure_1)
             ss = ss + ss1
-    print ('rez = ', ss)
+
     return ss
 
 
@@ -58,5 +50,5 @@ data_structure = [
 ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
-calculate_structure_sum(data_structure)
+print( calculate_structure_sum(data_structure) )
 
