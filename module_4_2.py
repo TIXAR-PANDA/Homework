8bde770e231284c6ac4bfb0f8d9b7a1ac3cfbb41
TIXAR-PANDA <natali040168@mail.ru>
2024-10-14 18:19:36 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,13 @@
+
+def test_function():
+    print ('----Работает test_function')
+
+    def inner_function():
+        print ("--------Я в области видимости функции test_function")
+
+    print ('----Вызываем inner_function')
+    inner_function()
+
+print('Работает основная программа ----')
+#inner_function() >>> NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
+test_function()
