85ca0906778076b5434ca61c052508207228f0e0
TIXAR-PANDA <natali040168@mail.ru>
2024-10-17 10:21:44 +0300

    классы,методы,атрибуты

---
+++ module_5_1.py
@@ -0,0 +1,25 @@
+# Атрибуты и методы объекта
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('"Такого этажа не существует"')
+        else:
+            for new_floor in range(new_floor+1):
+                if new_floor < 1:
+                    continue
+                print(new_floor)
+
+
+
+h1 = House('ЖК "Горский"', 18)
+h2 = House('Домик в деревне', 2)
+
+h1.go_to(4)
+h1.go_to(20)
+h2.go_to(-2)
+h2.go_to(2)
