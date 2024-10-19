fac49ae1f4c5c8ad76bcbe9ce442499e3688f106
TIXAR-PANDA <natali040168@mail.ru>
2024-10-19 10:38:13 +0300

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,33 @@
+# Различие атрибутов класса и экземпляра
+
+
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+    def __str__(self):
+        return f'Объект: {self.name} , этажность: {self.number_of_floors}'
+
+    def __del__(self):
+        print(f"{self}  - снесён, но он останется в истории")
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+
+print(House.houses_history)
+h2 = House('ЖК Акация', 12)
+h4 = House('Домик в деревне', 2)
+print(House.houses_history)
