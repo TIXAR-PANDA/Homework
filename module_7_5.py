fc50f5dabf856e5d8d8f6a80dc5d38187a7b52f4
TIXAR-PANDA <natali040168@mail.ru>
2024-11-09 14:29:47 +0300

    Update module_7_5.py

--- module_7_5.py
+++ module_7_5.py
@@ -15,22 +15,4 @@ for root, dirs, files in os.walk(directory):
               f'Путь: {filepath},',
               f'Размер: {filesize} байт,',
               f'Время изменения: {formatted_time},',
-              f'Родительская директория: {parent_dir}')
-
-
-"""
-Если хотим найти только текстовые файлы:
-print('Текущая директория:' , os.getcwd())
-directory = '.'
-for root, dirs, files in os.walk(directory):
-    for file in files:
-        if file.endswith(".txt"):
-            filepath = os.path.join(root, file)
-            filetime = os.path.getmtime(filepath)
-            formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-            filesize = os.path.getsize(filepath)
-            parent_dir = os.path.dirname(filepath)
-
-            print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, '
-                    f'Время изменения: {formatted_time}, Родительская директория: {parent_dir}') 
- """
+              f'Родительская директория: {parent_dir}')  
